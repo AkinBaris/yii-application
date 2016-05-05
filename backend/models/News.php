1e82c9f664b3ddab7422cb18fce19957b231ec41
AkinBaris <akinbaris1@gmail.com>
2016-05-05 16:16:22 +0300

    Alt yapı oluşturuldu
    Model ve CRUD oluşturuldu.Header a iki tane link eklendi.

---
+++ backend/models/News.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $category
+ * @property string $content
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'category', 'content'], 'required'],
+            [['category'], 'string'],
+            [['title'], 'string', 'max' => 100],
+            [['content'], 'string', 'max' => 200],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'category' => 'Category',
+            'content' => 'Content',
+        ];
+    }
+}
---
+++ frontend/web/backend/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\News */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="news-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'category')->dropDownList([ 'Bilgisayar' => 'Bilgisayar', 'Akıllı Cihazlar' => 'Akıllı Cihazlar', 'donanım' => 'Donanım', '' => '', ], ['prompt' => '']) ?>
+
+    <?= $form->field($model, 'content')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
